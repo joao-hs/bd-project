3c32d8d71b9984cccc2391db2300fd43d006d52c
bea-w <beatriz.matias@tecnico.ulisboa.pt>
2022-06-20 18:07:49 +0100

    debug view done

--- E3/view.sql
+++ E3/view.sql
@@ -3,6 +3,7 @@
 --      distrito e concelho: correspondem aos atributos com o mesmo nome de ponto_de_retalho
 --      ano, trimestre, mes, dia_mes e dia_semana: atributos derivado do atributo instante
 --      unidades: corresponde ao atributo com o emso nome da relação evento_reposição
+DROP VIEW vendas;
 
 CREATE VIEW Vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades)
 AS
@@ -14,20 +15,11 @@ SELECT
     to_char(instante, 'Mon'),
     to_char(instante, 'DD'),
     to_char(instante, 'D'),
-    --EXTRACT(YEAR FROM TIMESTAMP instante), 
-    --EXTRACT(QUARTER FROM TIMESTAMP instante), 
-    --EXTRACT(MONTH FROM TIMESTAMP instante), 
-    --EXTRACT(DAY FROM TIMESTAMP instante), 
-    --EXTRACT(DOW FROM TIMESTAMP instante), 
     pont_ret.distrito, 
     pont_ret.concelho,
     unidades
-FROM evento_reposicao as rep_event
-JOIN instalada_em AS instNS 
-    ON instNS.num_serie = rep_event.num_serie
-JOIN instalada_em AS instF 
-    ON instF.fabricante = instNS.fabricante
-JOIN ponto_de_retalho AS pont_ret 
-    ON pont_ret.ponto_de_retalho_nome = instF.ponto_de_retalho_nome
-JOIN tem_categoria AS has_cat 
-    ON has_cat.ean = rep_event.ean;
+    FROM evento_reposicao AS rep_event JOIN instalada_em AS inst
+    ON rep_event.num_serie = inst.num_serie AND rep_event.fabricante = inst.fabricante
+    JOIN ponto_de_retalho as pont_ret ON inst.ponto_de_retalho_nome = pont_ret.ponto_de_retalho_nome
+    JOIN tem_categoria as has_cat ON rep_event.ean = has_cat.ean;
+    
