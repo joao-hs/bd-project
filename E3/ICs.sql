2655bdd3ce6a1c95b216f660621dbce5f1db027a
bea-w <beatriz.matias@tecnico.ulisboa.pt>
2022-06-21 00:37:09 +0100

    ICS TODO

---
+++ E3/ICs.sql
@@ -0,0 +1,52 @@
+--- ICS ---
+-----------
+
+-- IC1 --
+CREATE TRIGGER category_check_trigger
+BEFORE INSERT ON tem_outra
+FOR EACH ROW EXECUTE PROCEDURE category_check_proc()
+
+CREATE OR REPLACE FUNCTION category_check_proc()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.super_categoria_nome = NEW.categoria_nome THEN
+        RAISE EXCEPTION '(IC-1) Uma Categoria não pode estar contida em si própria '
+    END IF;
+
+END;
+$$ LANGUAGE plpgsql;
+
+-- IC2 --
+CREATE TRIGGER max_units_trigger
+BEFORE INSERT ON evento_reposicao
+EXECUTE PROCEDURE max_units_proc();
+
+CREATE OR REPLACE FUNCTION max_units_proc()
+RETURNS TRIGGER AS
+$$
+DECLARE p_ean numeric(13);
+DECLARE num_p numeric(2);
+DECLARE num_s numeric(13);
+DECLARE fab varchar(80);
+DECLARE unit numeric(4);
+DECLARE cursor_planogram CURSOR FOR
+    SELECT ean, num_prateleira, num_serie, fabricante,unidades FROM planograma;
+BEGIN
+    OPEN cursor_planogram;     
+    LOOP
+        FETCH cursor_planogram INTO p_ean, num_p, num_s, fab, unit;
+        EXIT WHEN NOT FOUND;
+        IF NEW.ean = p_ean AND NEW.num_prateleira = num_p AND NEW.num_serie = num_s
+        AND NEW.fabricante = fab THEN
+            IF NEW.unidades > unit THEN
+              RAISE EXCEPTION '(IC-2) O número de unidades repostas num Evento de Reposição não pode exceder o número de unidades especificado no Planograma';
+            END IF;
+        END IF;
+    END LOOP; 
+    CLOSE cursor_planogram;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- IC3 --
