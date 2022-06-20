a7790d3a8dcd0a9ac0830467771789074d6811b8
JS-GHub <joaohsereno@tecnico.ulisboa.pt>
2022-06-20 02:15:34 +0100

    Correction of queries

--- E3/queries.sql
+++ E3/queries.sql
@@ -3,58 +3,53 @@
 
 SELECT retalhista_nome
 FROM (
-    SELECT tin
-    FROM (
-            SELECT COUNT(*)
-            FROM responsavelPor
-            GROUP BY tin
-    ) AS AUX
-    WHERE count = (
-        SELECT MAX(count)
-        FROM AUX
+    SELECT tin, COUNT(*)
+    FROM responsavel_por
+    GROUP BY tin
+    HAVING COUNT(*) >= ALL (
+        SELECT COUNT(*)
+        FROM responsavel_por
+        GROUP BY tin
     )
-) AS A JOIN retalhista AS R ON A.tid = R.tid;
+) AS aux JOIN retalhista ON aux.tin=retalhista.tin;
+
 
 
 -- 2 --
+SELECT retalhista_nome
+FROM(
+    SELECT tin, COUNT(*)
+    FROM responsavel_por as R RIGHT OUTER JOIN categoria_simples AS cs
+    ON r.categoria_nome=cs.categoria_simples_nome
+    GROUP BY tin
+    HAVING COUNT(*) = (
+        SELECT COUNT(*)
+        FROM categoria_simples
+    ) 
+)AS aux JOIN retalhista ON aux.tin=retalhista.tin;
 
-SELECT tin
-FROM (
-    SELECT categoria_nome, tin
-    FROM responsavelPor
-) AS a
-GROUP BY tin
-HAVING a.categoria_nome = (
-    SELECT categoriaSimples_nome
-    FROM categoriaSimples
-) 
+
+SELECT retalhista_nome, categoria_simples_nome 
+FROM
+retalhista AS r JOIN responsavel_por AS rp 
+ON r.tin=rp.tin
+RIGHT OUTER JOIN categoria_simples AS cs 
+ON rp.categoria_nome=cs.categoria_simples_nome;
+
 
 -- 3 --
 
 (SELECT ean FROM produto)
 EXCEPT
-(SELECT ean FROM eventoReposicao);
+(SELECT ean FROM evento_reposicao);
 
 
 -- 4 --
 
 SELECT ean
 FROM (
-        SELECT ean, COUNT(*)
-        FROM (
-                SELECT  ean, tid
-                FROM eventoReposicao
-        ) AS A
-        GROUP BY ean
-) AS B
-WHERE count = 1;
-
-
---- Experimentar para ver se dá
-SELECT ean
-FROM (
-    SELECT ean, tid
-    FROM eventoReposicao
-)
+    SELECT DISTINCT ean, tin
+    FROM evento_reposicao
+)AS a
 GROUP BY ean
 HAVING COUNT(*) = 1;
