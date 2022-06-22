36cb2987e2aef4e54bea76cb1275509f75e48ccc
bea-w <beatriz.matias@tecnico.ulisboa.pt>
2022-06-22 18:41:39 +0100

    Updated queries

--- E3/queries.sql
+++ E3/queries.sql
@@ -16,25 +16,10 @@ FROM (
 
 
 -- 2 --
-SELECT retalhista_nome
-FROM(
-    SELECT tin, COUNT(*)
-    FROM responsavel_por as R RIGHT OUTER JOIN categoria_simples AS cs
-    ON r.categoria_nome=cs.categoria_simples_nome
-    GROUP BY tin
-    HAVING COUNT(*) = (
-        SELECT COUNT(*)
-        FROM categoria_simples
-    ) 
-)AS aux JOIN retalhista ON aux.tin=retalhista.tin;
-
-
-SELECT retalhista_nome, categoria_simples_nome 
-FROM
-retalhista AS r JOIN responsavel_por AS rp 
-ON r.tin=rp.tin
-RIGHT OUTER JOIN categoria_simples AS cs 
-ON rp.categoria_nome=cs.categoria_simples_nome;
+SELECT DISTINCT retalhista_nome FROM
+retalhista AS r JOIN responsavel_por AS rp
+ON r.tin = rp.tin JOIN prateleira AS p 
+ON rp.fabricante = p.fabricante AND rp.num_serie = p.num_serie;
 
 
 -- 3 --
